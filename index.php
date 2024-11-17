6faa9942caf25b6fd3e0e1449e6339e6deadf348
teknik-github <mohamatraffi170@gmail.com>
2024-11-17 20:34:45 +0700

    bug

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ handleFileUpload($targetDirectory);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload File ke /mnt/nfs</title>
+    <title>Upload File ke /mnt/nfs bug</title>
 </head>
 <body>
     <h1>Upload File ke /mnt/nfs</h1>
